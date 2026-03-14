43ecc525a69248f327121c424b40851b1f95290e
PedroLucas26 <pedrolucas263@hotmail.com>
2026-03-14 18:28:46 -0300

    Cria uma entidade no banco de dados a partir de Tecnico model

---
+++ src/main/java/dev/java/GestaoOrdemDeServico/TecnicoModel.java
@@ -0,0 +1,51 @@
+package dev.java.GestaoOrdemDeServico;
+
+import jakarta.persistence.*;
+
+//Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "tb_cadastro_de_tecnicos")
+public class TecnicoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String idade;
+    private String cargo;
+
+    public TecnicoModel() {
+    }
+
+    public TecnicoModel(String nome, String idade, String cargo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+}
+
+}
