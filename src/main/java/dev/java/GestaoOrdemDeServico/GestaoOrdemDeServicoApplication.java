dcf0a9d2364a39b8d0c4f667f72e0015ef9a5cbe
PedroLucas26 <pedrolucas263@hotmail.com>
2026-03-14 14:23:53 -0300

    first commit

---
+++ src/main/java/dev/java/GestaoOrdemDeServico/GestaoOrdemDeServicoApplication.java
@@ -0,0 +1,13 @@
+package dev.java.GestaoOrdemDeServico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOrdemDeServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOrdemDeServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/GestaoOrdemDeServico/GestaoOrdemDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.GestaoOrdemDeServico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoOrdemDeServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
